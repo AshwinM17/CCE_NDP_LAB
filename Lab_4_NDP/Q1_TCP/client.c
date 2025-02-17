b8e2752d92f9fa0bbd78d5193c6f26abca0cb71c
AshwinM17 <ashwin.mittal3@gmail.com>
2025-02-17 20:04:12 +0530

    Lab 4 complete

--- Lab_4_NDP/Q1_TCP/client.c
+++ Lab_4_NDP/Q1_TCP/client.c
@@ -5,128 +5,74 @@
 #include <arpa/inet.h>
 
 #define PORT 8080
-#define MAX_BUFF 1024
+#define MAX_BUFFER 1024
 
-// Structure to store student information
-struct Student {
-    int registration_number;
-    char name[50];
-    char residential_address[100];
-    char department[50];
-    int semester;
-    char section;
-    char courses[200];
+// Structure to hold student information
+typedef struct
+{
+    char registration_number[20];
+    char name[100];
     char subject_code[10];
-    int marks;
-};
+} StudentInfo;
 
-void add_student(int sock) {
-    struct Student student;
-    printf("Enter Registration Number: ");
-    scanf("%d", &student.registration_number);
-    getchar();  // Consume newline character
-    printf("Enter Name: ");
-    fgets(student.name, sizeof(student.name), stdin);
-    student.name[strcspn(student.name, "\n")] = '\0';  // Remove newline
-    printf("Enter Residential Address: ");
-    fgets(student.residential_address, sizeof(student.residential_address), stdin);
-    student.residential_address[strcspn(student.residential_address, "\n")] = '\0';
-    printf("Enter Department: ");
-    fgets(student.department, sizeof(student.department), stdin);
-    student.department[strcspn(student.department, "\n")] = '\0';
-    printf("Enter Semester: ");
-    scanf("%d", &student.semester);
-    getchar();  // Consume newline character
-    printf("Enter Section: ");
-    scanf("%c", &student.section);
-    getchar();
-    printf("Enter Courses (comma separated): ");
-    fgets(student.courses, sizeof(student.courses), stdin);
-    student.courses[strcspn(student.courses, "\n")] = '\0';
-    printf("Enter Subject Code: ");
-    scanf("%s", student.subject_code);
-    printf("Enter Marks: ");
-    scanf("%d", &student.marks);
-
-    send(sock, &student, sizeof(student), 0);  // Send student details to server
-}
-
-void get_student_info(int sock) {
-    int reg_no;
-    printf("Enter Registration Number: ");
-    scanf("%d", &reg_no);
-    send(sock, &reg_no, sizeof(reg_no), 0);  // Send registration number to server
-
-    char buffer[MAX_BUFF];
-    int num_bytes = recv(sock, buffer, sizeof(buffer) - 1, 0);
-    if (num_bytes > 0) {
-        buffer[num_bytes] = '\0';
-        printf("Student Info: %s\n", buffer);
-    }
-}
-
-void update_student_marks(int sock) {
-    int reg_no, new_marks;
-    printf("Enter Registration Number: ");
-    scanf("%d", &reg_no);
-    printf("Enter New Marks: ");
-    scanf("%d", &new_marks);
-
-    send(sock, &reg_no, sizeof(reg_no), 0);  // Send registration number to server
-    send(sock, &new_marks, sizeof(new_marks), 0);  // Send new marks to server
-
-    char buffer[MAX_BUFF];
-    int num_bytes = recv(sock, buffer, sizeof(buffer) - 1, 0);
-    if (num_bytes > 0) {
-        buffer[num_bytes] = '\0';
-        printf("Response: %s\n", buffer);
-    }
-}
-
-int main() {
+int main()
+{
     int sock;
     struct sockaddr_in server_addr;
+    char buffer[MAX_BUFFER];
+    StudentInfo student_info;
 
-    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-        perror("Socket creation error");
-        return -1;
+    // Create a TCP socket
+    sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock < 0)
+    {
+        perror("Error creating socket");
+        exit(1);
     }
 
+    // Setup server address
+    memset(&server_addr, 0, sizeof(server_addr));
     server_addr.sin_family = AF_INET;
     server_addr.sin_port = htons(PORT);
-    if (inet_pton(AF_INET, "127.0.0.1", &server_addr.sin_addr) <= 0) {
-        perror("Invalid address");
-        return -1;
+    server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+    // Connect to the server
+    if (connect(sock, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
+    {
+        perror("Error connecting to server");
+        close(sock);
+        exit(1);
     }
 
-    if (connect(sock, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
-        perror("Connection failed");
-        return -1;
-    }
-
-    int option;
-    while (1) {
-        printf("Select an option:\n");
-        printf("1. Add Student\n");
-        printf("2. Get Student Info\n");
-        printf("3. Update Marks\n");
-        printf("4. Exit\n");
-        printf("Enter your choice: ");
+    while (1)
+    {
+        printf("\nEnter option (1: Registration Number, 2: Name, 3: Subject Code, 0: Exit): ");
+        int option;
         scanf("%d", &option);
+        getchar(); // Consume the newline character
 
-        send(sock, &option, sizeof(option), 0);  // Send option to server
-
-        if (option == 1) {
-            add_student(sock);
-        } else if (option == 2) {
-            get_student_info(sock);
-        } else if (option == 3) {
-            update_student_marks(sock);
-        } else if (option == 4) {
-            close(sock);
+        if (option == 0)
+        {
+            printf("Exiting...\n");
             break;
         }
+
+        snprintf(buffer, sizeof(buffer), "%d", option);
+        write(sock, buffer, strlen(buffer) + 1);
+
+        printf("Enter corresponding detail: ");
+        fgets(buffer, sizeof(buffer), stdin);
+        buffer[strcspn(buffer, "\n")] = 0;
+
+        if (option == 1) strcpy(student_info.registration_number, buffer);
+        else if (option == 2) strcpy(student_info.name, buffer);
+        else if (option == 3) strcpy(student_info.subject_code, buffer);
+
+        write(sock, &student_info, sizeof(student_info));
+        read(sock, buffer, sizeof(buffer));
+        printf("Server Response: %s\n", buffer);
     }
 
+    close(sock);
     return 0;
 }
--- Lab_4_NDP/Q1_TCP/server.c
+++ Lab_4_NDP/Q1_TCP/server.c
@@ -2,125 +2,204 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <sys/socket.h>
 #include <arpa/inet.h>
-#include <sys/types.h>
 #include <sys/wait.h>
 
-#define PORT 8080  // Use dynamic port assignment
-#define MAX_BUFF 1024
+#define PORT 8080
+#define MAX_BUFFER 1024
+#define MAX_STUDENTS 10
 
-// Structure to store student information
-struct Student {
-    int registration_number;
-    char name[50];
-    char residential_address[100];
-    char department[50];
-    int semester;
-    char section;
-    char courses[200];  // Comma-separated courses
+// Structure to hold student information
+typedef struct
+{
+    char registration_number[20];
+    char name[100];
     char subject_code[10];
-    int marks;
-};
+} StudentInfo;
 
-// Function to handle the "Add Student" operation
-void add_student(int new_sock, struct Student *database, int *db_size) {
-    struct Student student;
-    read(new_sock, &student, sizeof(student));  // Receive student details
+// Array to store multiple students' information
+StudentInfo students[MAX_STUDENTS];
+int student_count = 0;
 
-    // Add student to database
-    database[*db_size] = student;
-    (*db_size)++;
+// Function to add sample student entries
+void add_sample_students()
+{
+    strcpy(students[0].registration_number, "S101");
+    strcpy(students[0].name, "Alice");
+    strcpy(students[0].subject_code, "CS101");
 
-    send(new_sock, "Student added successfully!", 26, 0);
+    strcpy(students[1].registration_number, "S102");
+    strcpy(students[1].name, "Bob");
+    strcpy(students[1].subject_code, "CS102");
+
+    strcpy(students[2].registration_number, "S103");
+    strcpy(students[2].name, "Charlie");
+    strcpy(students[2].subject_code, "CS103");
+
+    student_count = 3; // Set the student count
 }
 
-// Function to handle the "Get Student Info" operation
-void get_student_info(int new_sock, struct Student *database, int db_size) {
-    int reg_no;
-    read(new_sock, &reg_no, sizeof(reg_no));  // Receive registration number
+// Function to handle client requests
+void handle_client(int client_sock)
+{
+    char buffer[MAX_BUFFER];
+    ssize_t bytes_read;
+    StudentInfo student_info;
 
-    // Search for student in database
-    for (int i = 0; i < db_size; i++) {
-        if (database[i].registration_number == reg_no) {
-            send(new_sock, &database[i], sizeof(struct Student), 0);
-            return;
-        }
-    }
-
-    send(new_sock, "Student not found.", 19, 0);
-}
-
-// Function to handle the "Update Student Marks" operation
-void update_student_marks(int new_sock, struct Student *database, int db_size) {
-    int reg_no, new_marks;
-    read(new_sock, &reg_no, sizeof(reg_no));  // Receive registration number
-    read(new_sock, &new_marks, sizeof(new_marks));  // Receive new marks
-
-    // Update marks in the database
-    for (int i = 0; i < db_size; i++) {
-        if (database[i].registration_number == reg_no) {
-            database[i].marks = new_marks;
-            send(new_sock, "Marks updated successfully!", 26, 0);
-            return;
-        }
-    }
-
-    send(new_sock, "Student not found.", 19, 0);
-}
-
-// Main server function
-int main() {
-    int server_fd, new_sock, db_size = 0;
-    struct sockaddr_in address;
-    char buffer[MAX_BUFF];
-    struct Student database[100];  // Simple database with a max of 100 students
-
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
-        perror("Socket failed");
-        exit(EXIT_FAILURE);
-    }
-
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons(PORT);
-
-    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
-        perror("Bind failed");
-        exit(EXIT_FAILURE);
-    }
-
-    if (listen(server_fd, 3) < 0) {
-        perror("Listen failed");
-        exit(EXIT_FAILURE);
-    }
-
-    printf("Server listening on port %d...\n", PORT);
-
-    while (1) {
-        if ((new_sock = accept(server_fd, NULL, NULL)) < 0) {
-            perror("Accept failed");
-            exit(EXIT_FAILURE);
-        }
-
-        // Read client option
-        int option;
-        read(new_sock, &option, sizeof(option));
-
-        if (option == 1) {
-            add_student(new_sock, database, &db_size);
-        } else if (option == 2) {
-            get_student_info(new_sock, database, db_size);
-        } else if (option == 3) {
-            update_student_marks(new_sock, database, db_size);
-        } else if (option == 4) {
-            send(new_sock, "Exiting...", 10, 0);
-            close(new_sock);
+    while (1) // Keep handling requests until the client disconnects
+    {
+        // Receive the client option
+        bytes_read = read(client_sock, buffer, MAX_BUFFER);
+        if (bytes_read <= 0)
+        {
+            printf("Client disconnected\n");
             break;
         }
 
-        close(new_sock);
+        int option = atoi(buffer);
+        if (option == 0) // Exit option from client
+        {
+            printf("Client requested to exit.\n");
+            break;
+        }
+
+        // Receive the student information
+        bytes_read = read(client_sock, &student_info, sizeof(StudentInfo));
+        if (bytes_read <= 0)
+        {
+            perror("Error reading student info from client");
+            break;
+        }
+
+        // Processing based on the selected option
+        int found = 0;
+        switch (option)
+        {
+        case 1: // Search by Registration Number
+            for (int i = 0; i < student_count; i++)
+            {
+                if (strcmp(students[i].registration_number, student_info.registration_number) == 0)
+                {
+                    snprintf(buffer, MAX_BUFFER, "Reg No: %s, Name: %s, Subject Code: %s, PID: %d",
+                             students[i].registration_number, students[i].name, students[i].subject_code, getpid());
+                    found = 1;
+                    break;
+                }
+            }
+            if (!found)
+                snprintf(buffer, MAX_BUFFER, "Student not found with Registration Number: %s", student_info.registration_number);
+            break;
+
+        case 2: // Search by Name
+            for (int i = 0; i < student_count; i++)
+            {
+                if (strcmp(students[i].name, student_info.name) == 0)
+                {
+                    snprintf(buffer, MAX_BUFFER, "Name: %s, Reg No: %s, Subject Code: %s, PID: %d",
+                             students[i].name, students[i].registration_number, students[i].subject_code, getpid());
+                    found = 1;
+                    break;
+                }
+            }
+            if (!found)
+                snprintf(buffer, MAX_BUFFER, "Student not found with Name: %s", student_info.name);
+            break;
+
+        case 3: // Search by Subject Code
+            for (int i = 0; i < student_count; i++)
+            {
+                if (strcmp(students[i].subject_code, student_info.subject_code) == 0)
+                {
+                    snprintf(buffer, MAX_BUFFER, "Subject Code: %s, Name: %s, Reg No: %s, PID: %d",
+                             students[i].subject_code, students[i].name, students[i].registration_number, getpid());
+                    found = 1;
+                    break;
+                }
+            }
+            if (!found)
+                snprintf(buffer, MAX_BUFFER, "Student not found with Subject Code: %s", student_info.subject_code);
+            break;
+
+        default:
+            snprintf(buffer, MAX_BUFFER, "Invalid Option");
+        }
+
+        // Send the response back to the client
+        write(client_sock, buffer, strlen(buffer) + 1);
     }
 
+    close(client_sock);
+    exit(0); // Terminate child process
+}
+
+int main()
+{
+    int server_sock, client_sock;
+    struct sockaddr_in server_addr, client_addr;
+    socklen_t client_len = sizeof(client_addr);
+
+    // Create a TCP socket
+    server_sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_sock < 0)
+    {
+        perror("Error creating socket");
+        exit(1);
+    }
+
+    // Setup server address
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+    server_addr.sin_port = htons(PORT);
+
+    // Bind the socket
+    if (bind(server_sock, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
+    {
+        perror("Error binding socket");
+        close(server_sock);
+        exit(1);
+    }
+
+    // Listen for connections
+    if (listen(server_sock, 5) < 0)
+    {
+        perror("Error listening on socket");
+        close(server_sock);
+        exit(1);
+    }
+
+    // Add sample students
+    add_sample_students();
+
+    printf("Server listening on port %d...\n", PORT);
+
+    while (1)
+    {
+        client_sock = accept(server_sock, (struct sockaddr *)&client_addr, &client_len);
+        if (client_sock < 0)
+        {
+            perror("Error accepting client connection");
+            continue;
+        }
+
+        // Create a child process to handle the client
+        pid_t pid = fork();
+        if (pid == 0) // Child process
+        {
+            close(server_sock);
+            handle_client(client_sock);
+        }
+        else if (pid > 0) // Parent process
+        {
+            close(client_sock);
+        }
+        else
+        {
+            perror("Error creating child process");
+            close(client_sock);
+        }
+    }
+
+    close(server_sock);
     return 0;
 }
